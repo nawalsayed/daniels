cd412a5724655c7ff3e26c05abb57bb526bdc65c
nawalsayed <nayamohammed255@gmail.com>
2025-06-24 17:11:40 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const navEL =document.querySelector('.navbar')
+window .addEventListener('scroll' ,()=>{
+    if(window.scrollY >=124){
+        navEL.classList.replace('navbar-default','navbar-scroll');
+    }
+
+}); 
